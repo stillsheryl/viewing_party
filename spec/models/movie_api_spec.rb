e3852fa40df93def1d3eb81627e30ee1f392b768
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-03 17:13:36 -0700

    Update test to include VCR cassettes

--- spec/models/movie_api_spec.rb
+++ spec/models/movie_api_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
-describe 'methods' do
+describe MovieApiService do
+  context "class methods" do
+    it ".conn" do
+      VCR.use_cassette('top_movies') do
+        api_response = MovieApiService.conn.get("/3/movie/top_rated")
+        expect(api_response.status).to eq(200)
+      end
+    end
 
-  it ".conn" do
-    api_response = MovieApiService.conn.get("/3/movie/top_rated")
-    expect(api_response.status).to eq(200)
-  end
+    it ".parse_data" do
+      VCR.use_cassette('top_movies') do
+        api_response = MovieApiService.conn.get("/3/movie/top_rated")
+        expect(MovieApiService.parse_data(api_response).class).to eq(Hash)
+      end
+    end
 
-  it ".parse_data" do
-    api_response = MovieApiService.conn.get("/3/movie/top_rated")
-    expect(MovieApiService.parse_data(api_response).class).to eq(Hash)
-  end
-
-  it ".top_rated_movies" do
-    expect(MovieApiService.top_rated_movies.class).to eq(Array)
-    expect(MovieApiService.top_rated_movies.count).to eq(40)
+    it ".top_rated_movies" do
+      VCR.use_cassette('top_movies') do
+        expect(MovieApiService.top_rated_movies.class).to eq(Array)
+        expect(MovieApiService.top_rated_movies.count).to eq(40)
+      end
+    end
   end
 end

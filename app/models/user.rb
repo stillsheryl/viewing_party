0311cc410571ea97f30265c8a6fb4644a1a037ca
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-04 19:13:56 -0700

    Add user model
    Co-authored-by: Zach Stearns 
    <64452036+Stearnzy@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,35 @@ class User < ApplicationRecord
 
   has_many :guests
   has_many :parties, through: :guests
+
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  has_many :users, foreign_key: :user_id, class_name: "Friendship"
+  has_many :friends, through: :users
+
+  has_many :friends, foreign_key: :friend_id , class_name: "Friendship"
+  has_many :users, through: :friends
 end
+
+
+
+
+
+# class User < ApplicationRecord
+# has_many :followers, foreign_key: :follower_id , class_name: "Friendship"
+# has_many :followed, through: :followers
+#
+# has_many :followed, foreign_key: :followed_id, class_name: "Friendship"
+# has_many :followers, through: :followed
+# end
+#
+# class Friendship < ApplicationRecord
+# belongs_to :follower, class_name: "User"
+# belongs_to :followed, class_name: "User"
+# end
+#
+# class Friendship < ApplicationRecord
+#   belongs_to :friend, class_name: "User"
+#   belongs_to :user, class_name: "User"
+# end

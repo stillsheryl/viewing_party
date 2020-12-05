fd6fd2d4f4ceb9e634ccc4903c1a886046aebdbd
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-04 19:12:52 -0700

    Add friendships table and model
    Co-authored-by: Zach Stearns 
    <64452036+Stearnzy@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_221814) do
+ActiveRecord::Schema.define(version: 2020_12_05_012707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "friend_id"
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "guests", force: :cascade do |t|
     t.bigint "party_id"
@@ -53,6 +62,8 @@ ActiveRecord::Schema.define(version: 2020_12_01_221814) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "guests", "parties"
   add_foreign_key "guests", "users"
   add_foreign_key "parties", "movies"

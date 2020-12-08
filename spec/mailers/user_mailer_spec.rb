843d5a6b6c2a146d2fd016ba161e6bab722465e8
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-08 16:43:07 -0700

    Update test setup for email creation

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,39 +2,28 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
 
-describe "when a user signs up for a new account" do
-  # it "send a welcome email" do
+describe "welcome_email" do
+      before :each do
+        @email_info = {
+          user: User.create!(
+            first_name: 'Zach',
+            last_name: 'Stearns',
+            email: 'zach@email.com',
+            password: 'thebestjedi'
+            ),
+          }
+      end
 
-      # user = User.create!(first_name: 'Shelly',
-      #   last_name: 'Waters',
-      #   email: 'shelly1@email.com',
-      #   password: 'password')
-      # email = UserMailer.welcome_email('no_reply@viewingparty.com', 'user@example.com', Time.now)
+    let(:mail) { UserMailer.welcome_email(@email_info, 'zach@email.com') }
 
-    # Send the email, then test that it got queued
-      # assert_emails 1 do
-      #   email.deliver_now
-      # end
-
-      # Test the body of the sent email contains what we expect it to
-      # assert_equal ['no_reply@viewingparty.com'], email.from
-      # assert_equal ['user@example.com'], email.to
-      # assert_equal 'Welcome to Viewing Party!', email.subject
-      # assert_equal read_fixture('invite').join, email.body.to_s
-
-
-    # let(:mail) { UserMailer.welcome_email.deliver_now }
-
-    xit "renders the headers" do
-      expect(mail.subject).to eq("Welcome to Viewing Party!")
-      expect(mail.to).to eq(["to@example.org"])
+    it "renders the headers" do
+      expect(mail.subject).to eq("Welcome to Viewing Party, Zach!")
+      expect(mail.to).to eq(["zach@email.com"])
       expect(mail.from).to eq(["no_reply@viewingparty.com"])
     end
 
-    xit "renders the body" do
+    it "renders the body" do
       expect(mail.body.encoded).to match("Welcome to ViewingParty.com")
     end
-
-    # end
   end
 end

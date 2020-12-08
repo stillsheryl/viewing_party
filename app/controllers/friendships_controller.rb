20c098a1e6f7c422f7bfc384562ee80e8800b827
Zach Stearns <zach.stearns92@gmail.com>
2020-12-07 19:05:36 -0700

    Refactor create action to make rubocop happy

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,16 +1,19 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:email])
-    unless friend.nil?
-      friendship = Friendship.find_by(user_id: current_user.id, friend_id: friend.id)
-    end
-    if friend.nil?
-      flash[:error] = "I'm sorry your friend cannot not found."
-    elsif friendship.nil?
+    if friend.nil? || current_user.friends.include?(friend)
+      error_message(friend)
+    else
       Friendship.create(user_id: current_user.id, friend_id: friend.id)
-    elsif !friendship.nil?
-      flash[:error] = 'You are already friends.'
     end
     redirect_to '/dashboard'
+  end
+
+  def error_message(friend)
+    if friend.nil?
+      flash[:error] = "I'm sorry your friend cannot not found."
+    elsif current_user.friends.include?(friend)
+      flash[:error] = 'You are already friends.'
+    end
   end
 end

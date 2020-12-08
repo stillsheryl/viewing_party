b47866c806ed97096282f9aac56f0f1e54827bcb
Zach Stearns <zach.stearns92@gmail.com>
2020-12-07 20:19:01 -0700

    Split generate_guests to helper method

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -11,18 +11,23 @@ class ViewingPartiesController < ApplicationController
                          date: params[:date],
                          time: params[:time],
                          duration: params[:duration_of_party])
-
     if party.save
-      Guest.create(party_id: party[:id], user_id: current_user.id, attending: true)
-      current_user.friends.each do |friend|
-        if params[:"input#friend-#{friend.id}"] == '1'
-          Guest.create(party_id: party[:id], user_id: friend.id, attending: false)
-        end
-      end
+      generate_guests(party)
       redirect_to '/dashboard'
     else
       flash[:error] = party.errors.full_messages.to_sentence
       render :new
+    end
+  end
+
+  private
+
+  def generate_guests(party)
+    Guest.create(party_id: party[:id], user_id: current_user.id, attending: true)
+    current_user.friends.each do |friend|
+      if params[:"input#friend-#{friend.id}"] == '1'
+        Guest.create(party_id: party[:id], user_id: friend.id, attending: false)
+      end
     end
   end
 end

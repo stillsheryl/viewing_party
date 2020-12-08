e238a2e970c3cd29054c4b7b20228fadca893d1a
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-08 14:26:13 -0700

    Update mailer to send message about viewing party

--- app/mailers/friend_notifier_mailer.rb
+++ app/mailers/friend_notifier_mailer.rb
@@ -1,13 +1,12 @@
 class FriendNotifierMailer < ApplicationMailer
   def inform(info, recipient)
     @user = info[:user]
-    # @message = info[:message]
     @friend = info[:friend]
 
     mail(
       reply_to: @user.email,
       to: recipient,
-      subject: "#{@user.name} is sending you some advice"
+      subject: "#{@user.first_name} has added you as a friend on ViewingParty!"
     )
   end
 end

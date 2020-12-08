f548c1a89899ba57c45be48e5c1770ec20f57451
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-08 14:28:38 -0700

    WIP - testing mailers

---
+++ spec/mailers/friend_notifier_mailer_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe FriendNotifierMailer, type: :mailer do
+
+  describe 'inform' do
+    sending_user = User.create(
+      first_name: 'Zach',
+      last_name: 'Stearns',
+      email: 'zach@email.com',
+      password: 'thebestjedi'
+    )
+
+    email_info = {
+      user: sending_user,
+      friend: 'Angelina',
+      message: 'Work through your anger with exercise, and wear a mask'
+    }
+
+    let(:mail) { FriendNotifierMailer.inform(email_info, 'angie@email.com') }
+
+    it 'renders the headers' do
+      # expect(ActionMailer::Base.deliveries.count).to eq(1)
+      # mail = ActionMailer::Base.deliveries.last
+
+      mail = mock(mail)
+      mail.should_receive(:deliver)
+      FriendNotifierMailer.should_receive(:inform).once.and_return(mail)
+
+      expect(mail.subject).to eq('Zach has added you as a friend on ViewingParty!')
+      expect(mail.to).to eq(['angie@email.com'])
+      expect(mail.from).to eq(['no_reply@viewingparty.com'])
+      expect(mail.reply_to).to eq(['zach@email.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.text_part.body.to_s).to include('Hello Angelina')
+      expect(mail.text_part.body.to_s).to include("I've added you as a freind on Viewing Party.com!")
+
+      expect(mail.html_part.body.to_s).to include('Hello Angelina')
+      expect(mail.html_part.body.to_s).to include("I've added you as a freind on Viewing Party.com!")
+
+      expect(mail.body.encoded).to include('Hello Angelina')
+      expect(mail.body.encoded).to include("I've added you as a freind on Viewing Party.com!")
+    end
+  end
+end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Friendship, type: :model do
     it { should belong_to :user }
     it { should belong_to(:friend).class_name('User') }
   end
+
+  it 'sends an email when a friendship is created' do
+    user1 = User.create!(first_name: 'Zach',
+                         last_name: 'Stearns',
+                             email: 'zach@email.com',
+                          password: 'password')
+    user2 = User.create!(first_name: 'Angelina',
+                            last_name: 'Jolie',
+                            email: 'angie@email.com',
+                            password: 'tombraider')
+
+    Friendship.create!(user_id: user1.id, friend_id: user2.id)
+
+
+    # mail = mock(mail)
+    # mail.should_receive(:deliver)
+    # FriendNotifierMailer.should_receive(:inform).once.and_return(mail)
+
+    expect { FriendNotifierMailer.inform }
+      .to change { ActionMailer::Base.deliveries.count }.by(1)
+  end
 end

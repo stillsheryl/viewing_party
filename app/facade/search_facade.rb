2d6dd2ab01bcec4c4b6e4afe50964699b8c579a0
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-09 18:09:36 -0500

    Added upcoming movies to facade

--- app/facade/search_facade.rb
+++ app/facade/search_facade.rb
@@ -11,4 +11,11 @@ class SearchFacade
   def self.top_rated_movies
     MovieApiService.top_rated_movies
   end
+
+  def self.upcoming_movies
+    movies = MovieApiService.upcoming_movies
+    movies.map do |movie|
+     UpcomingMovie.new(movie)
+    end
+  end
 end

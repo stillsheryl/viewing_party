bdb060f12544b7c903117bd46b341d9af57d811b
Zach Stearns <zach.stearns92@gmail.com>
2020-12-09 19:14:27 -0700

    Add test for viewing party info movie title being link to movie show page

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -10,7 +10,12 @@ describe 'As a user' do
     click_button 'Sign In'
 
     @current_user = User.last
-    @movie = Movie.create!(title: 'Avengers', api_id: 550)
+
+    VCR.use_cassette('retreive_avengers') do
+      @avengers = SearchFacade.movie_details('24428')
+    end
+
+    @movie = Movie.create!(title: "#{@avengers.title}", api_id: "#{@avengers.movie_id}")
     @party = Party.create!(user_id: @current_user.id, movie_id: @movie.id, date: '1/12/2020', time: '2:00 PM', duration: 220)
   end
 
@@ -67,10 +72,24 @@ describe 'As a user' do
     visit dashboard_path
 
     within id="#party-#{@party.id}" do
-      expect(page).to have_link('Avengers')
+      expect(page).to have_link("#{@movie.title}")
       expect(page).to have_content('December 1, 2020')
       expect(page).to have_content('2:00 PM')
       expect(page).to have_content('Invited')
+    end
+  end
+
+  it "Clicking on the movie name in the viewing party display takes me to the movie show page" do
+    Guest.create(party_id: @party.id, user_id: @current_user.id, attending: true)
+
+    VCR.use_cassette('get_movie_details') do
+      visit dashboard_path
+
+      within "#party-#{@party.id}" do
+        click_link("#{@movie.title}")
+      end
+
+      expect(current_path).to eq(movie_show_path("#{@avengers.movie_id}"))
     end
   end
 end
